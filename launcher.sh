cab8c3623749f3c92c502bba2b69d8a2c7d550af
kprotopopov <kprotopopov@pka.simtech>
2018-09-07 16:32:34 +0400

    Update for selenium

--- launcher.sh
+++ launcher.sh
@@ -18,26 +18,10 @@ getFolder () {
                     if [ "${folder}" = "${cscartMysqlConf}" ] || [ "${folder}" = "${cscartMysqlLogs}" ];then
                         mkdir -p -m777 ${folder}
                     else
-                        mkdir -p -m770 ${folder} && chown daemon: ${folder}
+                        mkdir -p -m755 ${folder} && chown www-data: ${folder}
                     fi
                 fi
             done
-        ;;
-
-        external)
-            folders="${cscartMysqlLogs} ${cscartMysqlConf} ${cscartVolumes}/cscart ${cscartVolumes}/acme ${cscartVolumes}/ssl"
-            for folder in ${folders}; do
-                if [[ ! -d "${folder}" ]]; then
-                    if [ "${folder}" = "${cscartMysqlConf}" ] || [ "${folder}" = "${cscartMysqlLogs}" ];then
-                        mkdir -p -m777 ${folder}
-                    elif [ "${folder}" = "${cscartVolumes}/acme" ] || [ "${folder}" = "${cscartVolumes}/ssl" ];then
-                        mkdir -p -m750 ${folder}
-                    else 
-                        mkdir -p -m770 ${folder} && chown daemon: ${folder}
-                    fi
-                fi
-            done
-
         ;;
     esac
 
@@ -55,21 +39,6 @@ getParams () {
             cscartMysqlData=$(cat .env | grep MYSQL_DATA | cut -f 2 -d =)
         ;;
 
-        external)
-            dockerCommand="docker-compose -f ./compose/global.yml -f ./compose/external.yml --project-name sandbox"
-            cscartVolumes=$(cat .env | grep CSCART_VOLUMES | cut -f 2 -d =)
-            cscartMysqlLogs=$(cat .env | grep MYSQL_LOGS | cut -f 2 -d =)
-            cscartMysqlConf=$(cat .env | grep MYSQL_CONF | cut -f 2 -d =)
-            cscartMysqlData=$(cat .env | grep MYSQL_DATA | cut -f 2 -d =)
-        ;;
-
-        wildcard)
-            wildcard=$(cat .env | grep WILDCARD | cut -f 2 -d =)
-            domain=$(cat .env | grep DOMAIN | cut -f 2 -d =)
-            cscartVolumes=$(cat .env | grep CSCART_VOLUMES | cut -f 2 -d =)
-            cert=${cscartVolumes}/ssl/${domain}.crt
-        ;;
-
         *)
             echo ""
             echo "Error!!! Not found ${var}."
@@ -84,16 +53,6 @@ doWork () {
     do=$1
     case ${do} in
 
-        external)
-            checkExist .env
-            getParams ${do}
-            getFolder ${do}
-            doWork wildcard
-            ${dockerCommand} up -d php56-fpm php70-fpm php71-fpm php72-fpm mysql 
-            sleep 3
-            ${dockerCommand} up -d 
-        ;;
-
         local)
             checkExist .env
             getParams ${do}
@@ -101,18 +60,6 @@ doWork () {
             ${dockerCommand} up -d php56-fpm php70-fpm php71-fpm php72-fpm mysql 
             sleep 3
             ${dockerCommand} up -d
-        ;;
-
-        wildcard)
-            checkExist .env
-            getParams ${do}
-            if [[ "${wildcard}" = "yes" ]]; then
-                ${dockerCommand} up -d proxy
-                while [ ! -f ${cert} ]; do
-                    echo " Please waiting for a certificate "
-                    sleep 10
-                done
-            fi
         ;;
 
         *)
